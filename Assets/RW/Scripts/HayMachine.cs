d9d19b11e56f2ac3753d3282ce75c348f5380184
Jinsong Liu <jinsong.liu01@estudiant.upf.edu>
2025-04-21 18:48:18 +0200

    Added shot and sheep

--- Assets/RW/Scripts/HayMachine.cs
+++ Assets/RW/Scripts/HayMachine.cs
@@ -8,14 +8,25 @@ public class HayMachine : MonoBehaviour
     public Vector3 velocity = Vector3.zero;
     private float limit = 22f;
 
+    public GameObject hayBalePrefab ; //Reference to the Hay Bale prefab.
+    public Transform haySpawnpoint ; //The point from which the hay will to be shot.
+    public float shootInterval ; //The smallest amount of time between shots
+    private float shootTimer ; //A timer that to keep track whether the machine can shoot
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shootTimer = shootInterval;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ShootHay()
+    {
+        Vector3 spawnPoint = haySpawnpoint.position;
+        spawnPoint.z -= 34;
+        Instantiate(hayBalePrefab , spawnPoint, Quaternion.identity);
+    }
+
+    private void manageMovement()
     {
         int direction = 0;
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -35,5 +46,22 @@ public class HayMachine : MonoBehaviour
         finalPosition.x = Mathf.Clamp(finalPosition.x, -limit, limit);
 
         transform.position = finalPosition;
+    }
+
+    private void manageProjectiles()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && (shootTimer > shootInterval))
+        {
+            ShootHay();
+            shootTimer = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootTimer += Time.deltaTime;
+        manageMovement();
+        manageProjectiles();
     }
 }
---
+++ Assets/RW/Scripts/SheepSpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepSpawner : MonoBehaviour
+{
+    public bool canSpawn = true;
+    public GameObject sheepPrefab ;
+    public List<Transform> sheepSpawnPositions = new List<Transform>();
+    public float timeBetweenSpawns ;
+    private List<GameObject> sheepList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        while (canSpawn)
+        {
+            SpawnSheep();
+            yield return new WaitForSeconds(timeBetweenSpawns);
+        }
+    }
+
+    public void RemoveSheepFromList(GameObject sheep)
+    {
+        sheepList.Remove(sheep);
+    }
+
+    private void SpawnSheep()
+    {
+        Vector3 randomPosition = sheepSpawnPositions[Random.Range(0,sheepSpawnPositions .Count)].position;
+        GameObject sheep = Instantiate(sheepPrefab, randomPosition, sheepPrefab.transform.rotation);
+        sheepList.Add(sheep);
+        sheep.GetComponent<Sheep>().SetSpawner(this);
+    }
+}
